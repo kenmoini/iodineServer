22c2278ab7710464b01d2fd59d37bd5f0b952247
Ken Moini <ken@kenmoini.com>
2019-05-13 20:41:01 -0500

    Update and rename python-builder.sh to install.sh
    -Added distribution checks for package installs
    -Removed Python venv - this is going to run in a container, no need for virtualenvs (someone correct me otherwise...)

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Sodium Builder
+# - This file will install all the needed packages, pull in AlgLib and install it
+
+# Switch between distributions
+if [ -x "$(command -v yum)" ]; then
+  # This is a CentOS/RHEL distro
+  yum install python3 wget git pip3 -y
+fi
+if [ -x "$(command -v apt)" ]; then
+  # This is a Debian/Ubuntu distro
+  # Pull in Python3 Virtual Environment packages
+  apt-get install python3 wget git pip3 -y
+fi
+
+pip3 install -r requirements.txt
+
+wget http://www.alglib.net/translator/re/alglib-3.15.0.cpython.free.zip
+
+unzip -o alglib-*.zip
+
+cd cpython && python3 ./setup.py install
+
+cd .. && rm -rf cpython && rm -rf alglib-*.zip
+
+echo 'Now just run: python3 ./rest-api.py -s'

d94bd377e77dd010baba6a3e3f5517a1f1f3731e
Ken Moini <ken@kenmoini.com>
2019-05-14 01:20:31 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@
 # Switch between distributions
 if [ -x "$(command -v yum)" ]; then
   # This is a CentOS/RHEL distro
-  yum install python2.7 wget git python-pip -y
+  yum install python2.7 wget git python-pip supervisor -y
 fi
 if [ -x "$(command -v apt)" ]; then
   # This is a Debian/Ubuntu distro
   # Pull in Python3 Virtual Environment packages
-  apt-get install python2.7 wget git python-pip -y
+  apt-get install python2.7 wget git python-pip supervisor -y
 fi
 
 pip3 install -r requirements.txt

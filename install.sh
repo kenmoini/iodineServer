b7934932acba5c786a455f33735760c1777098ad
Ken Moini <ken@kenmoini.com>
2019-05-14 11:04:56 -0500

    Update install.sh
    Added additional output context

--- install.sh
+++ install.sh
@@ -4,26 +4,55 @@
 # - This file will install all the needed packages, pull in AlgLib and install it
 
 # Switch between distributions
+
+echo ""
+echo "//=========================================================="
+echo "  Installing system packages..."
+echo "//=========================================================="
 if [ -x "$(command -v yum)" ]; then
   # This is a CentOS/RHEL distro
-  yum install python2.7 wget git python-pip supervisor -y
+  yum install python2.7 wget python-pip supervisor -y
 fi
 if [ -x "$(command -v apt)" ]; then
   # This is a Debian/Ubuntu distro
   # Pull in Python3 Virtual Environment packages
-  apt-get install python2.7 wget git python-pip supervisor -y
+  apt-get install python2.7 wget python-pip supervisor -y
 fi
 
+echo ""
+echo "//=========================================================="
+echo "  Creating supervisord conf.d directory if it doesn't exist"
+echo "//=========================================================="
 mkdir -p /etc/supervisor/conf.d/
 
+echo ""
+echo "//=========================================================="
+echo "  Installing pip modules"
+echo "//=========================================================="
 pip install -r requirements.txt
 
+echo ""
+echo "//=========================================================="
+echo "  Getting AlgLib 3.15 CPython Library"
+echo "//=========================================================="
 wget http://www.alglib.net/translator/re/alglib-3.15.0.cpython.free.zip
 
+echo ""
+echo "//=========================================================="
+echo "  Unzip AlgLib..."
+echo "//=========================================================="
 unzip -o alglib-*.zip
 
+echo ""
+echo "//=========================================================="
+echo "  Installing AlgLib..."
+echo "//=========================================================="
 cd cpython && python ./setup.py install
 
+echo ""
+echo "//=========================================================="
+echo "  Clean-up after downloads..."
+echo "//=========================================================="
 cd .. && rm -rf alglib-*.zip
 
 echo "//=========================================================="
